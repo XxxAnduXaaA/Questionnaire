10d02e0c6828dd246f8f55769cb88ae297c13a17
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-02 22:01:49 +0300

    feat(QuestionController.java): add QuestionController interface

--- src/main/java/com/example/formmaker/controller/QuestionController.java
+++ src/main/java/com/example/formmaker/controller/QuestionController.java
@@ -6,12 +6,13 @@ import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+@RequestMapping("/adminPanel")
 
 public interface QuestionController {
-    @PutMapping("/adminPanel/question/{questionId}")
+    @PutMapping("/question/{questionId}")
     public Question updateQuestion(@PathVariable @Min(1) Long questionId, @Valid @RequestBody Question updatedQuestion);
-    @DeleteMapping("/adminPanel/question/{questionId}")
+    @DeleteMapping("/question/{questionId}")
     public void deleteQuestion(@PathVariable @Min(1) Long questionId);
-    @GetMapping("/adminPanel/forms/{questionId}/questions")
+    @GetMapping("/forms/{questionId}/questions")
     public List<Question> findQuestionsByForm(@PathVariable @Min(1) Long questionId);
 }
