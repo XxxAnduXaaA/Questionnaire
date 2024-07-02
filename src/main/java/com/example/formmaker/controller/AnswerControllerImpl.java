05186fb3332ae8c5e7525703aba6e7dc335fb577
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-02 20:04:09 +0300

    feat(AnswerControllerImpl.java): add AnswerController implementation

---
+++ src/main/java/com/example/formmaker/controller/AnswerControllerImpl.java
@@ -0,0 +1,40 @@
+package com.example.formmaker.controller;
+
+import com.example.formmaker.entity.Answer;
+import com.example.formmaker.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AnswerControllerImpl implements AnswerController{
+
+    @Autowired
+    private AnswerService answerService;
+
+    @Override
+    public Answer addAnswerToQuestion(Long answerId, Answer answer) {
+        return answerService.addAnswerToQuestion(answerId, answer);
+    }
+
+    @Override
+    public Answer updateAnswer(Long answerId, Answer updatedAnswer) {
+        return answerService.updateAnswer(answerId, updatedAnswer);
+    }
+
+    @Override
+    public void deleteAnswerById(Long answerId) {
+        answerService.deleteAnswerById(answerId);
+    }
+
+    @Override
+    public List<Answer> getAnswersByQuestionId(Long answerId) {
+        return answerService.getAnswersByQuestionId(answerId);
+    }
+
+    @Override
+    public Answer getCorrectAnswerByFormId(Long answerId) {
+        return answerService.getCorrectAnswerByQuestionId(answerId);
+    }
+}
