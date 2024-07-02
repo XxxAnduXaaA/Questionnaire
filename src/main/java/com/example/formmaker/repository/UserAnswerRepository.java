e8c989c5e87561824b96a24bc6450bbcc80e44f9
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-02 17:59:35 +0300

    Added UserAnswerRepository

--- src/main/java/com/example/formmaker/repository/UserAnswerRepository.java
+++ src/main/java/com/example/formmaker/repository/UserAnswerRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
     List<UserAnswer> findByUserIdAndQuestionId(Long userId, Long questionId);
-    List<UserAnswer> findByUserIdAndFormId(Long userId, Long quizId);
+    List<UserAnswer> findByUserIdAndFormId(Long userId, Long formId);
     List<UserAnswer> findByUserId(Long userId);
 }
