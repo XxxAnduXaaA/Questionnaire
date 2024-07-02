7537a825adf08194af2e2b7709586bb8ec8f7673
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-02 20:08:32 +0300

    refactor(QuestionControllerImpl.java): change id names

--- src/main/java/com/example/formmaker/controller/QuestionControllerImpl.java
+++ src/main/java/com/example/formmaker/controller/QuestionControllerImpl.java
@@ -8,23 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class QuestionControllerImpl implements QuestionController{
+public class QuestionControllerImpl implements QuestionController {
 
     @Autowired
     private QuestionService questionService;
 
     @Override
-    public Question updateQuestion(Long id, Question updatedQuestion) {
-        return questionService.updateQuestion(id, updatedQuestion);
+    public Question updateQuestion(Long questionId, Question updatedQuestion) {
+        return questionService.updateQuestion(questionId, updatedQuestion);
     }
 
     @Override
-    public void deleteQuestion(Long id) {
-        questionService.deleteQuestion(id);
+    public void deleteQuestion(Long questionId) {
+        questionService.deleteQuestion(questionId);
     }
 
     @Override
-    public List<Question> findQuestionsByForm(Long id) {
-        return questionService.findQuestionsByForm(id);
+    public List<Question> findQuestionsByForm(Long questionId) {
+        return questionService.findQuestionsByForm(questionId);
     }
 }
