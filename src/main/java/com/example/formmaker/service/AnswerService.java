9b882454f2a1709b43314ef8cf60a9c5144da73d
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-02 20:07:23 +0300

    refactor(AnswerService.java): change id names

--- src/main/java/com/example/formmaker/service/AnswerService.java
+++ src/main/java/com/example/formmaker/service/AnswerService.java
@@ -5,9 +5,10 @@ import com.example.formmaker.entity.Answer;
 import java.util.List;
 
 public interface AnswerService {
-    public Answer addAnswerToQuestion(Long question_id, Answer answer);
-    public Answer updateAnswer(Long question_id, Answer updatedAnswer);
-    public void deleteAnswerById(Long answer_id);
-    public List<Answer> getAnswersByQuestionId(Long question_id);
-    public List<Answer> getCorrectAnswersByFormId(Long form_id);
+    public Answer addAnswerToQuestion(Long questionId, Answer answer);
+    public Answer updateAnswer(Long questionId, Answer updatedAnswer);
+    public void deleteAnswerById(Long answerId);
+    public List<Answer> getAnswersByQuestionId(Long questionId);
+//    public List<Answer> getCorrectAnswersByFormId(Long form_id);  TODO: Добавить в form
+    public Answer getCorrectAnswerByQuestionId(Long questionId);
 }
