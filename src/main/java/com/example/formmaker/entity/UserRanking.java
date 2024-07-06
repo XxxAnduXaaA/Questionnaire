d06fa80cb0400f03f109ba3e32b6fcaa5f3f1098
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-06 21:57:58 +0300

    feat(UserRanking.java): add UserRanking entry's incrementation

--- src/main/java/com/example/formmaker/entity/UserRanking.java
+++ src/main/java/com/example/formmaker/entity/UserRanking.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Entity
 public class UserRanking {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long rankingId;
 
