8a3fc99649eb68787ba2c45ae869f03f7d7b0e2d
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-06 19:09:39 +0300

    feat(Question.java): add Question entry's id incrementation fix(Question.java): add annotations @JsonBackReference and @JsonManagedReference to avoid recursion

--- src/main/java/com/example/formmaker/entity/Question.java
+++ src/main/java/com/example/formmaker/entity/Question.java
@@ -1,21 +1,30 @@
 package com.example.formmaker.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Table(name = "question_table")
 @Data
 @Entity
 public class Question {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long questionId;
 
     @ManyToOne
     @JoinColumn(name = "form_id", nullable = false)
-    private Form formId;
+    @JsonBackReference
+    private Form form;
 
     @Column
     private String questionText;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    List<Answer> answers;
 }
