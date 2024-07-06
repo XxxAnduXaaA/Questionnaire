99ee89688839694dd3e0b516d46192089235bd29
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-06 19:09:59 +0300

    feat(User.java): add User entry's id incrementation

--- src/main/java/com/example/formmaker/entity/User.java
+++ src/main/java/com/example/formmaker/entity/User.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column
     private Long id;
 
