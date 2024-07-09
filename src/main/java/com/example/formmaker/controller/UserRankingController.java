9a113c5e18e7919265312a3606c6569a1fe295e9
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-09 13:46:02 +0300

    fix(UserRankingController.java): add PathVariable annotation feat(UserRankingController.java): add id validation

--- src/main/java/com/example/formmaker/controller/UserRankingController.java
+++ src/main/java/com/example/formmaker/controller/UserRankingController.java
@@ -1,16 +1,18 @@
 package com.example.formmaker.controller;
 
 import com.example.formmaker.entity.UserRanking;
+import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
 @RequestMapping("/user")
 public interface UserRankingController {
     @GetMapping("/{userId}/rank")
-    public UserRanking getRankUserById(Long userId);
+    public UserRanking getRankUserById(@PathVariable @Min(1) Long userId);
     @GetMapping("/getAllRanks")
     public List<UserRanking> getAllRanks();
 }
