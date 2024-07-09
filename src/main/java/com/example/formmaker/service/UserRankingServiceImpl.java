56d9854296cadaa573196ea2e42e24c0d81cd6c2
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-09 13:46:49 +0300

    fix(UserRankingService.java): add AutoWired annotation for userRankingRepository

--- src/main/java/com/example/formmaker/service/UserRankingServiceImpl.java
+++ src/main/java/com/example/formmaker/service/UserRankingServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 public class UserRankingServiceImpl implements UserRankingService{
 
-
+    @Autowired
     private UserRankingRepository userRankingRepository;
 
     @Override
