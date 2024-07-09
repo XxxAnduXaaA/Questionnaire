c4fbb0c1d4404803a70ed680f17dd7170bf489d7
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-09 13:40:38 +0300

    refactor(AnswerController.java): fix errors in variable names

--- src/main/java/com/example/formmaker/controller/AnswerController.java
+++ src/main/java/com/example/formmaker/controller/AnswerController.java
@@ -9,14 +9,14 @@ import java.util.List;
 
 @RequestMapping("/adminPanel")
 public interface AnswerController {
-    @PostMapping("/question/{answerId}/answer")
-    public Answer addAnswerToQuestion(@PathVariable @Min(1) Long answerId, @Valid @RequestBody Answer answer);
+    @PostMapping("/question/{questionId}/answer")
+    public Answer addAnswerToQuestion(@PathVariable @Min(1) Long questionId, @Valid @RequestBody Answer answer);
     @PutMapping("/answer/{answerId}")
     public Answer updateAnswer(@PathVariable @Min(1) Long answerId, @Valid @RequestBody Answer updatedAnswer);
     @DeleteMapping("/answer/{answerId}")
     public void deleteAnswerById(@PathVariable @Min(1) Long answerId);
-    @GetMapping("/question/{answerId}/getAnswers")
-    public List<Answer> getAnswersByQuestionId(@PathVariable @Min(1) Long answerId);
-    @GetMapping("/question/{answerId}/getCorrectAnswer")
-    public Answer getCorrectAnswerByQuestionId(@PathVariable @Min(1) Long answerId);
+    @GetMapping("/question/{questionId}/getAnswers")
+    public List<Answer> getAnswersByQuestionId(@PathVariable @Min(1) Long questionId);
+    @GetMapping("/question/{questionId}/getCorrectAnswer")
+    public Answer getCorrectAnswerByQuestionId(@PathVariable @Min(1) Long questionId);
 }
