b9728ac878973a9dfba60859dd1a0815b6a8023f
XxxAnduXaaA <nekit991tka2017@gmail.com>
2024-07-09 13:55:00 +0300

    feat(FormServiceImpl.java): add userAnswerRepository to correct delete method

--- src/main/java/com/example/formmaker/service/FormServiceImpl.java
+++ src/main/java/com/example/formmaker/service/FormServiceImpl.java
@@ -2,9 +2,12 @@ package com.example.formmaker.service;
 
 import com.example.formmaker.entity.Form;
 import com.example.formmaker.entity.Question;
+import com.example.formmaker.entity.UserAnswer;
 import com.example.formmaker.repository.FormRepository;
+import com.example.formmaker.repository.UserAnswerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +17,9 @@ public class FormServiceImpl implements FormService{
 
     @Autowired
     private FormRepository formRepository;
+
+    @Autowired
+    private UserAnswerRepository userAnswerRepository;
 
     public Form createForm(Form form){
         return formRepository.save(form);
@@ -36,7 +42,33 @@ public class FormServiceImpl implements FormService{
             Form existingForm = optionalForm.get();
             existingForm.setDescription(updatedForm.getDescription());
             existingForm.setTitle(updatedForm.getTitle());
-            //TODO:реализовать изменение каждого вопроса
+
+//            List<Question> existingQuestions = existingForm.getQuestions();
+//            List<Question> updatedQuestions = updatedForm.getQuestions();
+//
+//            int i = 0;
+//
+//            while(i < existingQuestions.size() && i < updatedQuestions.size()){
+//
+//                existingForm.getQuestions().get(i).setQuestionText(updatedForm.getQuestions().get(i).getQuestionText());
+//                existingForm.getQuestions().get(i).setAnswers(updatedForm.getQuestions().get(i).getAnswers());
+//                i++;
+//
+//            }
+//
+//            if(i < updatedQuestions.size()){ //для большего updatedQuestions
+//                while(i < updatedQuestions.size()){
+//                    repository.save(integers1.get(i));
+//                    addQuestionToForm(formId, updatedQuestions.get(i));
+//                    i++;
+//                }
+//            }
+//            if(i < existingQuestions.size()){ //для большего existingQuestions
+//                while(i < existingQuestions.size()){
+//                    existingQuestions.remove(i);
+//                    questionsRepository.deleteById(existingQuestions.get(i).getQuestionId());
+//                }
+//
 
             return formRepository.save(existingForm);
         }
@@ -51,7 +83,7 @@ public class FormServiceImpl implements FormService{
         if(optionalForm.isPresent()){
             Form existingForm = optionalForm.get();
             existingForm.getQuestions().add(question);
-            question.setFormId(existingForm);
+            question.setForm(existingForm);
 
             return formRepository.save(existingForm);
         }
@@ -59,8 +91,11 @@ public class FormServiceImpl implements FormService{
         throw new RuntimeException("Form not found with id" + formId);
     }
 
+    @Transactional
     @Override
     public void deleteFormById(Long formId) {
+        List<UserAnswer> userAnswers = userAnswerRepository.findAllByQuestion_Form_FormId(formId);
+        userAnswerRepository.deleteAll(userAnswers);
         formRepository.deleteById(formId);
     }
 }
