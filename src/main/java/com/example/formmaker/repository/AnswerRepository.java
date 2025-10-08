452caab57481c6817f171eddb57e57d9fdda7aad
Nikita Nadysev <115927491+XxxAnduXaaA@users.noreply.github.com>
2025-10-08 23:44:10 +0400

    Delete src directory

--- src/main/java/com/example/formmaker/repository/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.formmaker.repository;
-
-import com.example.formmaker.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    void deleteAllByQuestion_QuestionId(Long id);
-}
