a12515233bcb386b14cb6e14f6658b900739b3cd
AndromedA <nekit911tka2017@gmail.co>
2025-10-30 18:11:02 +0300

    Отформатировал

--- src/main/java/com/example/formmaker/FormmakerApplication.java
+++ src/main/java/com/example/formmaker/FormmakerApplication.java
@@ -3,8 +3,6 @@ package com.example.formmaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class FormmakerApplication {
 
