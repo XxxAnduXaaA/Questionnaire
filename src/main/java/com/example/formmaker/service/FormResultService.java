eba0202b9cb96dce8e0a78ed98196749bec6e721
AndromedA <nekit911tka2017@gmail.co>
2025-11-06 22:12:59 +0300

    Добавил методы getFormResults, modelFiller и getUserFormPage в интерфейс

--- src/main/java/com/example/formmaker/service/FormResultService.java
+++ src/main/java/com/example/formmaker/service/FormResultService.java
@@ -1,9 +1,11 @@
 package com.example.formmaker.service;
 
+import com.example.formmaker.dto.FormResultsRequestDto;
 import com.example.formmaker.entity.Form;
 import com.example.formmaker.entity.FormResult;
 import com.example.formmaker.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.ui.Model;
 
 
 public interface FormResultService {
@@ -14,6 +16,11 @@ public interface FormResultService {
 
     Page<FormResult> getCompletedFormsByForm(int page, int size, Long formId);
 
+    Page<FormResult> getFormResults(FormResultsRequestDto formResultsRequestDt);
+
+    void modelFiller(Model model, Page<FormResult> results, int page);
+
     FormResult createFormResult(User user, Form form);
 
+    void getUserFormPage(Long userFormId, Model model);
 }
