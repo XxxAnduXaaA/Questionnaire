f55e146d6ac8020de57ea07f7f7e1322489701ea
AndromedA <nekit911tka2017@gmail.co>
2025-11-06 22:10:25 +0300

    Добавил @RequiredArgsConstructor

--- src/main/java/com/example/formmaker/controller/IndexController.java
+++ src/main/java/com/example/formmaker/controller/IndexController.java
@@ -2,21 +2,19 @@ package com.example.formmaker.controller;
 
 import com.example.formmaker.security.MyUserDetails;
 import com.example.formmaker.service.FormService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 @PreAuthorize("hasAuthority('ROLE_USER')")
 public class IndexController {
 
     private final FormService formService;
-
-    public IndexController(FormService formService) {
-        this.formService = formService;
-    }
 
     @GetMapping("/")
     public String indexPage(Model model, @AuthenticationPrincipal MyUserDetails userDetails){
@@ -24,5 +22,4 @@ public class IndexController {
         model.addAttribute("user", userDetails.getUser());
         return "index";
     }
-
 }
