dff8734821aff31c1c7009b21a7f8cad0a39d5e1
AndromedA <nekit911tka2017@gmail.co>
2025-11-06 17:28:09 +0300

    Перенёс всё в сервис и выделил аргументы функции getFormsResult в отдельный класс

--- src/main/java/com/example/formmaker/controller/AdminFormController.java
+++ src/main/java/com/example/formmaker/controller/AdminFormController.java
@@ -31,34 +31,13 @@ public class AdminFormController {
 
     @GetMapping("/form/new")
     public String getFormCreator(Model model) {
-        Form form = new Form();
-        Question q = new Question();
-        Answer a = new Answer();
-
-        form.setQuestions(new ArrayList<>());
-        form.getQuestions().add(q);
-
-        q.setForm(form);
-        q.setAnswers((new ArrayList<>()));
-        q.getAnswers().add(a);
-
-        a.setQuestion(q);
-
+        Form form = formService.createBlankForm();
         model.addAttribute("form", form);
         return "admin/form-create";
     }
 
     @PostMapping("/form/new")
     public String createForm(@ModelAttribute Form form) {
-        if (formService.getFormByTitle(form.getTitle()).isPresent()) {
-            throw new EntityExistsException("Форма с таким названием уже существует");
-        }
-        for (Question q : form.getQuestions()) {
-            q.setForm(form);
-            for (Answer a : q.getAnswers()) {
-                a.setQuestion(q);
-            }
-        }
         formService.createForm(form);
         return "redirect:/admin-panel/form";
     }
@@ -82,40 +61,15 @@ public class AdminFormController {
     }
 
     @GetMapping("/form/results")
-    public String getFormsResult(@RequestParam(value = "formId", required = false) Long formId, @RequestParam(value = "userId", required = false) Long userId, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, Model model) {
-
-        Page<FormResult> results;
-
-        if (formId != null) {
-//            model.addAttribute("formResults", formResultService.getCompletedFormsByForm(page, size, formId));
-            results = formResultService.getCompletedFormsByForm(page, size, formId);
-        } else if (userId != null) {
-//            model.addAttribute("userResults", formResultService.getCompletedFormsByUser(page, size, userId));
-            results = formResultService.getCompletedFormsByUser(page, size, userId);
-        } else {
-            results = formResultService.getAllCompletedForms(page, size);
-        }
-
-//        model.addAttribute("allFormResults", formResultService.getAllCompletedForms(page, size));
-        model.addAttribute("results", results);
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", results.getTotalPages());
-        model.addAttribute("forms", formService.getAllForms());
-        model.addAttribute("users", userRepository.findAll());
+    public String getFormsResult(@ModelAttribute FormResultsRequestDto formResultsRequestDto, Model model) {
+        Page<FormResult> results = formResultService.getFormResults(formResultsRequestDto);
+        formResultService.modelFiller(model, results, results.getTotalPages());
         return "admin/formsResult";
     }
 
     @GetMapping("/form/results/{userFormId}")
     public String getUserFormPage(@PathVariable Long userFormId, Model model) {
-        FormResult formResult = formResultRepository
-                .findById(userFormId)
-                .orElseThrow(() -> new RuntimeException("FormResult with " + userFormId + " not found"));
-        model.addAttribute("form", formResult.getForm());
-        List<UserAnswer> userAnswers = formResult.getAnswers();
-        Map<Long, List<UserAnswer>> answersByQuestion = userAnswers.stream()
-                .collect(Collectors.groupingBy(ua -> ua.getQuestion().getQuestionId()));
-
-        model.addAttribute("answersByQuestion", answersByQuestion);
+        formResultService.getUserFormPage(userFormId, model);
         return "user/userForm";
     }
 }
