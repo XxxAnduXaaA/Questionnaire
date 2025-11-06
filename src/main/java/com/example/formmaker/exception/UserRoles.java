5c14677b503e175af3beb0c376ccf7d6cc554386
AndromedA <nekit911tka2017@gmail.co>
2025-11-06 17:44:30 +0300

    Вынес в отдельный класс роли пользователей

---
+++ src/main/java/com/example/formmaker/exception/UserRoles.java
@@ -0,0 +1,6 @@
+package com.example.formmaker.exception;
+
+public final class UserRoles {
+    public final static String ADMIN = "ROLE_ADMIN";
+    public final static String USER = "ROLE_USER";
+}
