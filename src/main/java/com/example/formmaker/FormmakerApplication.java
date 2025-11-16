50da28070350c72c6aa512f31a479906349c3d99
AndromedA <nekit911tka2017@gmail.co>
2025-11-16 21:26:44 +0300

    Добавил аудит

--- src/main/java/com/example/formmaker/FormmakerApplication.java
+++ src/main/java/com/example/formmaker/FormmakerApplication.java
@@ -2,12 +2,13 @@ package com.example.formmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FormmakerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FormmakerApplication.class, args);
     }
-
 }
