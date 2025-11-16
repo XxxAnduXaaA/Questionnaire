b757202a2f08fe9dbe6b7ca69c480f82226c7bd0
AndromedA <nekit911tka2017@gmail.co>
2025-11-16 21:48:53 +0300

    добавил @RequiredArgsConstructor

--- src/main/java/com/example/formmaker/security/MyUserDetailsService.java
+++ src/main/java/com/example/formmaker/security/MyUserDetailsService.java
@@ -2,11 +2,13 @@ package com.example.formmaker.security;
 
 import com.example.formmaker.entity.User;
 import com.example.formmaker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
