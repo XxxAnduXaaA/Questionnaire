403ce01d30de7acc0af5f09644536597d9425a36
AndromedA <nekit911tka2017@gmail.co>
2025-11-16 21:38:20 +0300

    Добавил исключение для null значений в FormResult

--- src/main/java/com/example/formmaker/exception/FormResultException.java
+++ src/main/java/com/example/formmaker/exception/FormResultException.java
@@ -2,7 +2,11 @@ package com.example.formmaker.exception;
 
 public final class FormResultException extends RuntimeException {
 
-    public static RuntimeException notFound(Long id){
+    public static RuntimeException NOT_FOUND(Long id) {
         return new RuntimeException(String.format("Form result with id %d not found", id));
+    }
+
+    public static RuntimeException DATA_IS_NULL() {
+        return new RuntimeException("user или answer не должны быть null");
     }
 }
